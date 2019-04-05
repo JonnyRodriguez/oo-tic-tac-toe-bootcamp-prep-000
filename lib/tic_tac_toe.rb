c2be262102677a0247973e13c398f857722aff1a
Jonny Rodriguez <jojoroto@gmail.com>
2019-04-05 15:43:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,24 +1,24 @@
 class TicTacToe
   def initialize(board=nil)
-    @board = board if board
+    @board = board || Array.new(9, " ")
+    @WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+  end
 
-WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+  def input_to_index(input)
+    input.to_i-1
+  end
 
-def input_to_index(input)
-  input.to_i-1
-end
-
-def move(board,index,player)
-  board[index]=player
-end
+  def move(index,player)
+    @board[index]=player
+  end
 
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
