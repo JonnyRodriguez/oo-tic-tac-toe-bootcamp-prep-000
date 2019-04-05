71545709277d47094050b15566e896fb0968f3b9
Jonny Rodriguez <jojoroto@gmail.com>
2019-04-05 15:54:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,10 +1,7 @@
-
-
 class TicTacToe
   WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
   def initialize(board=nil)
     @board = board || Array.new(9, " ")
-    @WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
   end
 
   def display_board
@@ -28,7 +25,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    !position_taken?(@board,index) if index.between?(0,8)
+    !position_taken?(index) if index.between?(0,8)
   end
 
   def turn
